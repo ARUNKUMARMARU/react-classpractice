7850020cf25b90bd44f159488b14fd7ec3db81a9
ARUNKUMARMARU <arunsmart1020@gmail.com>
2023-10-19 17:06:26 +0530

    adding minus button

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,20 @@ function App() {
   const addValue = () => {
     setcounter(counter + 1);
   }
+
+  const zeroValue = () => {
+    setcounter(0);
+  }
+
+  const reduceValue = () => {
+    setcounter(counter - 1);
+  }
   return (
   <div>{counter}
   <div>
     <button onClick={addValue}>Add</button>
+    <button onClick={zeroValue}>Zero</button>
+    <button onClick={ reduceValue}>Subract</button>
   </div>
   </div>
   )
