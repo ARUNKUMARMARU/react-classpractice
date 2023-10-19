52c273b50bfdd278ea0842970066c14767ef01f7
ARUNKUMARMARU <arunsmart1020@gmail.com>
2023-10-19 13:37:39 +0530

    adding state counter using evevt listener

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import React, { useState } from 'react'
 function App() {
   const [counter, setcounter] = useState(0);
 
-  setTimeout(() => {
-    setcounter(counter+1)
-  },);
+  const addValue = () => {
+    setcounter(counter + 1);
+  }
   return (
-  <div>{counter}</div>
+  <div>{counter}
+  <div>
+    <button onClick={addValue}>Add</button>
+  </div>
+  </div>
   )
 }
 
