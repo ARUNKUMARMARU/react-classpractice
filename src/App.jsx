5b86cc05a8aac470003decb55090b8e302429af1
ARUNKUMARMARU <arunsmart1020@gmail.com>
2023-10-26 11:47:53 +0530

    useRef hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,19 @@
-import React, { useState } from 'react'
+// import { useRef } from "react";
+import React, {useRef} from 'react'
+
 
 function App() {
-  const [counter, setcounter] = useState(0);
-
-  const addValue = () => {
-    setcounter(counter + 1);
-  }
-
-  const zeroValue = () => {
-    setcounter(0);
-  }
-
-  const reduceValue = () => {
-    setcounter(counter - 1);
-  }
+    const inputRef = useRef(null);
+    const handleButtonClick = () => {
+        inputRef.current.focus();
+    }
   return (
-  <div>{counter}
-  <div>
-    <button onClick={addValue}>Add</button>
-    <button onClick={zeroValue}>Zero</button>
-    <button onClick={ reduceValue}>Subract</button>
-  </div>
-  </div>
+    
+    <div>
+        <input ref={inputRef}>
+        </input>
+        <button onClick={handleButtonClick} >enter</button>
+    </div>
   )
 }
 
